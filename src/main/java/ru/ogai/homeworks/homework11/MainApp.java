513ef5631f9aa0bf1ab90078049bf353bc741a6e
Vlada Ogai <vladamun0663@gmail.com>
2024-03-10 20:37:50 +0300

    удалила sout

--- src/main/java/ru/ogai/homeworks/homework11/MainApp.java
+++ src/main/java/ru/ogai/homeworks/homework11/MainApp.java
@@ -2,7 +2,6 @@ package ru.ogai.homeworks.homework11;
 
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("hello!");
-        System.out.println("hi");
+
     }
 }
