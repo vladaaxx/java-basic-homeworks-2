718d7d3065397255d682918584e23f7bca1490c5
Vlada Ogai <vladamun0663@gmail.com>
2024-03-10 20:34:13 +0300

    добавила sout

--- src/main/java/ru/ogai/homeworks/homework11/MainApp.java
+++ src/main/java/ru/ogai/homeworks/homework11/MainApp.java
@@ -2,6 +2,7 @@ package ru.ogai.homeworks.homework11;
 
 public class MainApp {
     public static void main(String[] args) {
-
+        System.out.println("hello!");
+        System.out.println("hi");
     }
 }
